3e1f0e70f4892758bf774e83219408556b41f947
Kalid Warsame <kalid.warsame@connect.qut.edu.au>
2021-08-24 17:17:54 +1000

    pdf

---
+++ SWIM/SWIM/ViewModels/PdfViewerModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SWIM.Models;
+using Xamarin.Forms;
+
+namespace SWIM.ViewModels
+{
+    public class PdfViewerModel : INotifyPropertyChanged
+    {
+        private Stream m_pdfDocumentStream;
+
+        /// <summary>
+        /// An event to detect the change in the value of a property.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The PDF document stream that is loaded into the instance of the PDF viewer. 
+        /// </summary>
+        public Stream PdfDocumentStream
+        {
+            get
+            {
+                return m_pdfDocumentStream;
+            }
+            set
+            {
+                m_pdfDocumentStream = value;
+                NotifyPropertyChanged("PdfDocumentStream");
+            }
+        }
+
+        /// <summary>
+        /// Constructor of the view model class
+        /// </summary>
+        public PdfViewerModel()
+        {
+            //Accessing the PDF document that is added as embedded resource as stream.
+            m_pdfDocumentStream = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("SAMPLE BILL.pdf");
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
